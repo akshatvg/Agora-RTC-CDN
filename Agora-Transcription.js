2e34f66fb30729bae4e6dee1507cd684c0e86a4a
Akshat Gupta <akshatvg23@gmail.com>
2020-08-01 03:45:41 +0530

    transcription.js

---
+++ Agora-Transcription.js
@@ -0,0 +1,39 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+var Content = '';
+
+recognition.continuous = true;
+
+recognition.onresult = function (event) {
+
+    var current = event.resultIndex;
+
+    var transcript = event.results[current][0].transcript;
+
+    Content += transcript;
+    document.getElementById("chatbox").innerHTML = Content;
+
+};
+
+recognition.onstart = function () {
+    console.log('Voice recognition is on.');
+}
+
+recognition.onspeechend = function () {
+    console.log('No activity found.');
+}
+
+recognition.onerror = function (event) {
+    if (event.error == 'no-speech') {
+        console.log('Could you please repeat? I didn\'t get what you\'re saying.');
+    }
+}
+
+$(function () {
+    if (Content.length) {
+        Content += ' ';
+    }
+    recognition.start();
+});
