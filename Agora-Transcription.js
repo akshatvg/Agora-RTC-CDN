8956679da46f991ed7e4db24fc63174a0e69701b
Akshat Gupta <akshatvg23@gmail.com>
2020-08-02 20:32:47 +0530

    recognition

--- Agora-Transcription.js
+++ Agora-Transcription.js
@@ -23,13 +23,13 @@ recognition.onstart = function () {
 
 recognition.onspeechend = function () {
     console.log('No activity found.');
-    recognition.onstart();
+    recognition.start();
 }
 
 recognition.onerror = function (event) {
     if (event.error == 'no-speech') {
         console.log('Could you please repeat? I didn\'t get what you\'re saying.');
-        recognition.onstart();
+        recognition.start();
     }
 }
 
