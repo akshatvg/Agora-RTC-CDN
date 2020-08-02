b8e244a08dbbd466f0400a1dce92eaf2302824e0
Akshat Gupta <akshatvg23@gmail.com>
2020-08-02 22:29:30 +0530

    trial

--- reloadOnce.js
+++ reloadOnce.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     if (location.href.indexOf('reload') == -1) {
         location.href = location.href + '?reload';
+        window.location.reload(true);
     }
 });
