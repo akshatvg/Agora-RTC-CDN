03b09786eaae4a513b2c668437c5538b2ba4917d
Akshat Gupta <akshatvg23@gmail.com>
2020-08-02 22:32:28 +0530

    test

--- reloadOnce.js
+++ reloadOnce.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     if (location.href.indexOf('reload') == -1) {
-        window.location.reload();
         location.href = location.href + '?reload';
+        window.location.reload();
     }
 });
